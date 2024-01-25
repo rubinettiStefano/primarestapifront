1b52d9028a40e89219d168189d29fd31e0880482
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-01-25 12:28:36 +0100

    v2

--- homepage.js
+++ homepage.js
@@ -1,5 +1,40 @@
 let people = [];
 
+function insertPerson()
+{
+    //sto "trasformando" la form in un oggetto anonimo
+    let toInsert = 
+    {
+        name:document.getElementById("inName").value,
+        surname:document.getElementById("inSurname").value,
+        job:document.getElementById("inJob").value,
+        dob:document.getElementById("inDob").value
+    };
+
+    let jsonizzato = JSON.stringify(toInsert);
+
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: jsonizzato
+    };
+
+    fetch("http://localhost:8080/people", requestOptions)
+    .then(response => response.text())
+    .then
+    (
+        body =>{
+                    let personaAppenaInserita = JSON.parse(body);
+                    people.push(personaAppenaInserita);
+                    init();
+        }
+        
+    )
+}
+
 let init = function()
 {
     let divPeople = document.getElementById("peoplelist");
@@ -31,10 +66,11 @@ let requestOptions =
 fetch("http://localhost:8080/people", requestOptions)//fai una request GET all'URLhttp://localhost:8080/people
 .then(response => response.text())//prende la response e della response prende solo il BODY
 .then
-(body => {
-            people = JSON.parse(body);
-            init();
-         }
+(
+    body => {
+                people = JSON.parse(body);
+                init();
+            }
 )// il body della response
                                              //con JSON.parse convertiamo un json in un vettore di oggetti JAVASCRIPT
 
